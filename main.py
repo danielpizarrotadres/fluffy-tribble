83543be9944e31a4c07b04ce86eb57875ba82366
Daniel <118082275+danielpizarrotadres@users.noreply.github.com>
2024-06-30 17:41:39 -0400

    :ambulance: Clean the seats already in storage

--- main.py
+++ main.py
@@ -4,12 +4,12 @@ from seat_search_engine import seat_search_engine
 
 cabin_seats = [
     {"seat": "1A", "row": 1, "column": "A", "status": "AVAILABLE"},
-    {"seat": "1B", "row": 1, "column": "B", "status": "NOT_AVAILABLE"},
-    {"seat": "1C", "row": 1, "column": "C", "status": "AVAILABLE"},
-    {"seat": "1D", "row": 1, "column": "D", "status": "AVAILABLE"},
-    {"seat": "1E", "row": 1, "column": "E", "status": "AVAILABLE"},
-    {"seat": "1F", "row": 1, "column": "F", "status": "AVAILABLE"},
-    {"seat": "2A", "row": 2, "column": "A", "status": "AVAILABLE"},
+    {"seat": "1B", "row": 1, "column": "B", "status": "AVAILABLE"},
+    {"seat": "1C", "row": 1, "column": "C", "status": "NOT_AVAILABLE"},
+    {"seat": "1D", "row": 1, "column": "D", "status": "NOT_AVAILABLE"},
+    {"seat": "1E", "row": 1, "column": "E", "status": "NOT_AVAILABLE"},
+    {"seat": "1F", "row": 1, "column": "F", "status": "NOT_AVAILABLE"},
+    {"seat": "2A", "row": 2, "column": "A", "status": "NOT_AVAILABLE"},
     {"seat": "2B", "row": 2, "column": "B", "status": "NOT_AVAILABLE"},
     {"seat": "2C", "row": 2, "column": "C", "status": "NOT_AVAILABLE"},
     {"seat": "2D", "row": 2, "column": "D", "status": "NOT_AVAILABLE"},
@@ -33,20 +33,28 @@ doubles = try_match_doubles(passengers)
 def assign_seats(groups, available_seats):
     availability = True
     seats_results = []
-    for i, passengers in enumerate(groups):
+    all_available_seats = [group.copy() for group in available_seats]
+    for passengers in groups:
         total_passengers = len(passengers)
         candidate_seats = []
-        for k, seats in enumerate(available_seats):
+        for seats in all_available_seats:
             if len(seats) >= total_passengers:
-                    candidate_seats.append(seats)
+                candidate_seats.append(seats)
         total_candidates = len(candidate_seats)
         if total_candidates == 0:
             availability = False
             break
         matched_seats = seat_search_engine(candidate_seats, total_passengers)
         seats_results.append(matched_seats)
-    return seats_results
-
+        for matched_seat in matched_seats:
+            for seats_group in all_available_seats:
+                if matched_seat in seats_group:
+                    seats_group.remove(matched_seat)
+                    break
+    if availability:
+        return seats_results
+    else:
+        return []
 
 result = assign_seats(doubles, available_seats)
 print(result)
