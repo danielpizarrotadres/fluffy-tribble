e8267eaff52c64d3f75c9768889a007858f4182f
Daniel <118082275+danielpizarrotadres@users.noreply.github.com>
2024-07-17 15:27:57 -0400

    :construction: Work in progress

--- script.py
+++ script.py
@@ -24,20 +24,18 @@ def fetch_data_from_reservation_order(pnr):
         scraper = cloudscraper.create_scraper()
         url = "https://reservation-order.skyairline.com/v1/order/find-by-pnr"
         payload = {"pnr": pnr }
-        print(payload)
         response = scraper.post(url, json=payload)
         response.raise_for_status()
         return response.json()
     except Exception as err:
         # TODO: Make something
-        return None
+        return None 
 
 def find_flight(itinerary_parts, flight_number, origin, departure_date):
     for itinerary_part in itinerary_parts:
         for segment in itinerary_part['segments']:
             departure_year, departure_month, departure_day = segment['departureDate'].split('T')[0].split('-')
             formatted_departure_date = f"{departure_year}-{departure_month}-{departure_day}"
-        
             if (segment['flightNumber']['marketing'] == flight_number and
                 segment['origin'] == origin and
                 formatted_departure_date == departure_date):
@@ -47,21 +45,18 @@ affected_flights = find_affected_flights()
 
 for index, flight in enumerate(affected_flights):
     print(f"Current {index + 1}: {flight['origin']} {flight['flightNumber']} {flight['departureDate']} / Total pnrs: {len(flight['affectedPnrs'])}")
-
     for pnr in flight['affectedPnrs']:
         data_from_reservation_order = fetch_data_from_reservation_order(pnr)
-
         if data_from_reservation_order:
             itinerary_parts = data_from_reservation_order.get('itineraryParts', [])
-
             matching_segment = find_flight(itinerary_parts, flight['flightNumber'], flight['origin'], flight['departureDate'])
-
             if matching_segment:
-                print(f"Matching segment for PNR {pnr}: {matching_segment}")
-
-                data_for_excel.append({"PNR": pnr, "Flight": f"{flight['origin']} {flight['flightNumber']} {flight['departureDate']}"})
+                data_for_excel.append({
+                    "PNR": pnr,
+                    "Affected Flight": f"{flight['origin']} {flight['flightNumber']} {flight['departureDate']}",
+                    "Flight From Order": f"{matching_segment['origin']} {matching_segment['flightNumber']['marketing']} {matching_segment['departureDate']}",
+                })
 
 
 df = pd.DataFrame(data_for_excel)
 df.to_excel("affected_flights.xlsx", index=False, sheet_name="Affected Flights")
-
