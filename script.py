477932d081d2149ec209cce6926bb5009a4d5eef
Daniel <118082275+danielpizarrotadres@users.noreply.github.com>
2024-07-19 14:49:46 -0400

    :construction: WIP

--- script.py
+++ script.py
@@ -4,6 +4,9 @@ import cloudscraper
 import pandas as pd
 import os
 
+green = "\033[92m"
+reset = "\033[0m"
+
 db_url = 'mongodb+srv://affected_notification_request_prd_user:UXHwBpyoIGWVNr1P@itinerarios.yiqqc.mongodb.net/affected_notification_request_prd_db?retryWrites=true&w=majority&appName=ITINERARIOS'
 db_name = 'affected_flights_request_prd_db'
 db_collection = 'affectations'
@@ -15,8 +18,8 @@ scraper = cloudscraper.create_scraper()
 def find_affected_flights():
     db = client[db_name]
     collection = db[db_collection]
-    # query = {"departureDate": {"$gte": "2024-07-17"}, "affectedPnrs": {"$ne": []}}
-    query = {"flightNumber": 285, "departureDate": "2025-01-01", "origin": "ANF"}
+    query = {"departureDate": {"$gte": "2024-07-17"}, "affectedPnrs": {"$ne": []}}
+    # query = {"flightNumber": 285, "departureDate": "2025-01-01", "origin": "ANF"}
     documents = collection.find(query)
     return list(documents)
 
@@ -52,24 +55,45 @@ def find_flight(flight, itinerary_parts):
     hash_flight = f"{flight['origin']}-{flight['destination']}-{flight['departureDate']}-{flight['carrier']}-{str(flight['flightNumber']).zfill(4)}"
     for itinerary_part in itinerary_parts:
         for segment in itinerary_part['segments']:
+            print(segment['departureDate'])
+            print(flight['scheduledDepartureTime'])
             if (segment['hash'] == hash_flight) and segment['departureDate'] == flight['scheduledDepartureTime']:
                 return segment
     return None
 
 affected_flights = find_affected_flights()
+data_for_excel = []
 
 for i, flight in enumerate(affected_flights):
-    print(f"Current Flight {i + 1} of total {len(affected_flights)}:")
+    print(f"{green}Current Flight {i + 1} of total {len(affected_flights)}:{reset}")
     for j, pnr in enumerate(flight['affectedPnrs']):
+        temp_data = {
+            "Pnr": pnr,
+            "Affected Flight": "",
+            "New Flight (Protector)": "",
+        }
         reservation_order = fetch_order(pnr)
         if reservation_order:
-            print(f"- Order: {reservation_order['orderId']} / total flights: {len(reservation_order.get('itineraryParts', []))}")
+            print(f"    - Pnr: {pnr} / Order: {reservation_order['orderId']} / total flights: {len(reservation_order.get('itineraryParts', []))}")
             itinerary_parts = reservation_order.get('itineraryParts', [])
             affected_flight = find_flight(flight['oldFlight'], itinerary_parts)
             if affected_flight:
+                temp_data['Affected Flight'] = affected_flight['hash']
                 reservation = fetch_reservation(pnr)
                 if reservation:
                     itinerary_parts = reservation.get('itineraryParts', [])
                     new_flight = find_flight(flight['newFlight'], itinerary_parts)
-                    print(new_flight)
-                    pass
+                    if new_flight:
+                        print(f"{green}Pnr has affected flight in order and new flight in host:{reset}")
+                        temp_data['New Flight (Protector)'] = new_flight['hash']
+                        data_for_excel.append(temp_data)
+
+output_directory = '/home/daniel/dev/py-helpers'
+output_file = os.path.join(output_directory, "affected_flights.xlsx")
+
+try:
+    df = pd.DataFrame(data_for_excel)
+    df.to_excel(output_file, index=False, sheet_name="Affected Flights-Finished")
+    print(f"Excel file saved successfully at {output_file}")
+except Exception as e:
+    print(f"Failed to save Excel file: {e}")
