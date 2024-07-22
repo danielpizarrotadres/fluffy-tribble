01f0144b5e85324d39053b90650f7478c75b34d8
Daniel <118082275+danielpizarrotadres@users.noreply.github.com>
2024-07-22 10:16:08 -0400

    Update notifications.py

--- notifications.py
+++ notifications.py
@@ -61,7 +61,6 @@ def find_affected_flight(aux, flight, itinerary_parts):
         departureDate = aux['departureDate']
         flightNumber = aux['flightNumber']
     hash_flight = f"{origin}-{flight['destination']}-{departureDate}-{flight['carrier']}-{str(flightNumber).zfill(4)}"
-    print(f"hash_flight: {hash_flight}")
     for itinerary_part in itinerary_parts:
         for segment in itinerary_part['segments']:
             if (segment['hash'] == hash_flight):
@@ -79,8 +78,9 @@ def host_is_unsync(aux, flight, itinerary_parts):
     hash_flight = f"{origin}-{flight['destination']}-{departureDate}-{flight['carrier']}-{str(flightNumber).zfill(4)}"
     for itinerary_part in itinerary_parts:
         for segment in itinerary_part['segments']:
-            if (segment['hash'] == hash_flight) and (segment['departureDate'] != flight['scheduledDepartureTime']):
-                return segment
+            if (segment['hash'] == hash_flight):
+                if (segment['departureDate'] != flight['scheduledDepartureTime']):
+                    return segment
     return None
 
 def update_manual_notified(flight):
@@ -89,11 +89,22 @@ def update_manual_notified(flight):
     new_values = {"$set": {"manualNotified": True}}
     db[db_collection].update_one(query, new_values)
 
+def send_notification(payload):
+    print(f"Sending notification for flight: ")
+    try:
+        url = "https://check-in-publisher.skyairline.com/v1/publish-message"
+        response = scraper.post(url, json=payload)
+        response.raise_for_status()
+        data = response.json()
+        response.close()
+        return data
+    except Exception as err:
+        print(f'Error into send_notification for pnr: {pnr}')
+        print(err)
+        return None
 
 affected_flights = find_affected_flights()
 data_for_excel = []
-
-print(f"Total affected flights: {len(affected_flights)}")
 
 for i, flight in enumerate(affected_flights):
     print(f"{green}Current Flight {i + 1} of total {len(affected_flights)}:{reset}")
@@ -120,6 +131,80 @@ for i, flight in enumerate(affected_flights):
                         print(f"{green}Pnr has affected flight in order and unsync schedule in host:{reset}")
                         temp_data['New Flight (Protector)'] = new_flight['hash']
                         data_for_excel.append(temp_data)
+                        data = {
+                            "routingKey": "communication.intinerary.changed",
+                            "channel": "itinerary-history-request",
+                            "message": {
+                                "homemarket": "CL",
+                                "language": "es",
+                                "pnr": pnr,
+                                "origin": flight['oldFlight']['origin'],
+                                "destination": flight['oldFlight']['origin'],
+                                "departureDate": "2024-10-25T09:30:00",
+                                "passengers": [
+                                    {
+                                        "id": 1,
+                                        "firstName": "CESIA",
+                                        "lastName": "GUIZADA",
+                                        "email": "",
+                                        "type": "",
+                                        "gender": "Male",
+                                        "dateOfBirth": "0085-01-31",
+                                        "documentInfo": {
+                                            "issuingCountry": "PE",
+                                            "documentNumber": "42801336",
+                                            "documentType": "I",
+                                            "nationality": "PE"
+                                        }
+                                    }
+                                ],
+                                "orderContact": [
+                                    {
+                                        "manager": {
+                                            "firstName": "CESIA",
+                                            "lastName": "GUIZADA",
+                                            "documentNumber": "42801336",
+                                            "documentType": "I"
+                                        },
+                                        "email": [
+                                            {
+                                                "value": "daniel.pizarro@skyairline.com"
+                                            }
+                                        ],
+                                        "phones": []
+                                    }
+                                ],
+                                "details": {
+                                    "pnr": "LFXLQM",
+                                    "agency": False,
+                                    "retail": True,
+                                    "groups": False,
+                                    "before72": False,
+                                    "in72": True,
+                                    "mmbLink": "https://mmb.skyairline.com/es/chile",
+                                    "wciLink": "https://check-in.skyairline.com/es/chile",
+                                    "yellowAlertLink": "https://www.skyairline.com/chile/formularios/contactanos",
+                                    "oldFlight": {
+                                        "departureCity": "SCL",
+                                        "arrivalCity": "ZAL",
+                                        "arrivalTime": "09:30",
+                                        "departureDate": "2024-10-25",
+                                        "flightNumber": 111,
+                                        "departureTime": "09:30",
+                                        "duration": 90
+                                    },
+                                    "newFlight": {
+                                        "departureCity": "SCL",
+                                        "arrivalCity": "ZAL",
+                                        "arrivalTime": "09:30",
+                                        "departureDate": "2024-10-25",
+                                        "flightNumber": 111,
+                                        "departureTime": "09:30",
+                                        "duration": 90
+                                    }
+                                }
+                            }
+                        }
                         update_manual_notified(flight)
 
 
